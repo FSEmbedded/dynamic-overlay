7fa9d9b253bc84f5b959ab0cce7ebd59edc01ed5
Goetz Grimmer <grimmer@fs-net.de>
2021-08-18 14:23:11 +0000

    Add: Help to configure manually dynamic-overlay
    This script is helping building dynamic-mounting during manual
    development.

---
+++ manual_configure.sh
@@ -0,0 +1,6 @@
+cmake . -DRAUC_SYSTEM_CONF_PATH=/etc/rauc/system.conf \
+		-DNAND_RAUC_SYSTEM_CONF_PATH=/etc/rauc/system.conf.nand \
+		-DEMMC_RAUC_SYSTEM_CONF_PATH=/etc/rauc/system.conf.mmc \
+		-DUBOOT_ENV_PATH=/etc/fw_env.config \
+		-DNAND_UBOOT_ENV_PATH=/etc/fw_env.config.nand \
+		-DEMMC_UBOOT_ENV_PATH=/etc/fw_env.config.mmc
